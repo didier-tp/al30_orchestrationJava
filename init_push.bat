c0b8a21f82dac8d3b398773092e939ebfbb3e34a
didier <didier@iciOuLa.fr>
2017-12-05 10:42:22 +0100

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add al30_orchestrationJava https://didier-tp:pwd007!@github.com/didier-tp/al30_orchestrationJava.git
+git remote add al30_orchestrationJava https://didier-tp:....!@github.com/didier-tp/al30_orchestrationJava.git
 git push -u al30_orchestrationJava master
 pause
 
